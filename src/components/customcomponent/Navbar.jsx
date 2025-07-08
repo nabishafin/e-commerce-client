8efc19647625eee9ac1a0dcdfab422125366e72f
nabishafin <shafin21215@gmail.com>
2025-07-08 19:07:43 +0600

    ui update

--- src/components/customcomponent/Navbar.jsx
+++ src/components/customcomponent/Navbar.jsx
@@ -19,12 +19,12 @@ export function Navbar() {
   }
 
   return (
-    <nav className="sticky top-0 z-50 w-full px-2 md:px-10 bg-gray-950/10 backdrop-blur supports-[backdrop-filter]:bg-gray-900/60 shadow-sm">
+    <nav className="sticky top-0 z-50 w-full px-2 md:px-10 bg-white text-black  shadow-sm">
       <div className="flex h-16 items-center justify-between px-4">
         {/* Logo / Title */}
         <Link to={"/"}>
           <div className="flex items-center space-x-4">
-            <h1 className="text-2xl font-bold text-white">Mini E-Commerce</h1>
+            <h1 className="text-2xl font-bold ">Mini E-Commerce</h1>
           </div>
         </Link>
 
@@ -32,7 +32,7 @@ export function Navbar() {
         <div className="flex items-center space-x-4 relative">
           {/* Home */}
           <Link to={"/"}>
-            <Button variant="ghost" size="sm" className="flex items-center space-x-2 text-white hover:bg-gray-100">
+            <Button variant="ghost" size="sm" className="flex items-center space-x-2  hover:bg-gray-100">
               <Home className="h-4 w-4" />
               <span className="hidden sm:inline">Home</span>
             </Button>
@@ -43,7 +43,7 @@ export function Navbar() {
             variant="ghost"
             size="sm"
             onClick={handleCartClick}
-            className="relative flex items-center space-x-2 text-white hover:bg-white"
+            className="relative flex items-center space-x-2  hover:bg-white"
           >
             <ShoppingCart className="h-4 w-4" />
             <span className="hidden sm:inline">Cart</span>
