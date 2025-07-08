4bd4dae488a4e3e4397fa4944b45a58b9dbb4cd8
nabishafin <shafin21215@gmail.com>
2025-07-08 17:24:26 +0600

    try to deploy

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  baseUrl: "https://e-commerce-server-tawny-psi.vercel.app",
   prepareHeaders: (headers, { getState }) => {
     headers.set("Content-Type", "application/json");
     return headers;
